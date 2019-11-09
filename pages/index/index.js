3311766c9b9d492e4cdd6b204cdff53fcc5ee175
huangziqian <huangziqian@onion-inc.cn>
2019-11-09 19:17:52 +0800

    完成入场动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,13 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var wxDraw = require("../../utils/wxdraw.min.js").wxDraw;
+var Shape = require("../../utils/wxdraw.min.js").Shape;
+var AnimationFrame = require("../../utils/wxdraw.min.js").AnimationFrame;
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    tarbarItems: [
+    tarbarItems:[
       {
         "key": "home",
         "text": "首页",
@@ -22,7 +21,11 @@ Page({
         "selectedIconPath": "../../assets/icons/mine_active.png"
       }
     ],
-    currentTab: 0
+    currentTab: 0,
+    isCanvasShow: 0,
+    wxCanvas: null,
+    animateColor: ["#4285f4", "#ea4335", "#fbbc05", "#5961F9", "#4285f4", "#34a853", "#ea4335", "#C32BAC", "#F067B4", "#1904E5", "#81FFEF",
+     "#3C8CE7", "#FFE985", "#FFE985"]
   },
   //事件处理函数
   tabbarChange: function(e) {
@@ -35,40 +38,58 @@ Page({
       })
     }
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  bindtouchmove: function (e) {
+    // 检测手指点击 之后的移动事件
+    this.wxCanvas.touchmoveDetect(e);
+  },
+  bindtouchend: function () {
+    //检测手指点击 移出事件
+    this.wxCanvas.touchendDetect();
+  },
+  bindtap: function (e) {
+    this.wxCanvas.tapDetect(e);
+  },
+  bindlongpress: function (e) {
+    // console.log(e);
+    this.wxCanvas.longpressDetect(e);
+  },
+  sayingsInit: function (sayings) {
+    var textArr = []
+    var colorArr = this.data.animateColor
+    for(var i= 0;i < sayings.length; i++) {
+      var text = new Shape('text', {
+        text: sayings[i],
+        x: 50 + 30 * parseInt(i),
+        y: 400,
+        fontSize: 30,
+        fillStyle: colorArr[i]
+      }, 'fill', false)
+      this.wxCanvas.add(text)
+      textArr.push(text)
+    }
+    for (var i = 0; i < sayings.length; i++) {
+      (function (j) {
+        setTimeout(function () {
+          textArr[j].animate({ "y": "-=100" }, { easing: "swingTo", duration: 1000 }).animate({ "y": "+=100" }, { easing: "swingFrom", duration: 1000 }).start(true)
+        }, 300 + 50 * j);
+      })(i)
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  entryAnimation: function () {
+    var context = wx.createCanvasContext('enteryCanvas')
+    this.wxCanvas = new wxDraw(context, 0, 0, 400, 500);
+    var sayings ="世上无难事，只要肯放弃" 
+    this.sayingsInit(sayings)
+  },
+  onLoad: function () {
+    this.entryAnimation()
+    setTimeout(() => {
+      this.setData({
+        isCanvasShow: 1
+      })
+      this.wxCanvas.clear(); //推荐这个
+    }, 3500)
+  },
+  onUnload: function () {
   }
 })
